1d92725b3a4be84a83e55cc9682407f468405f65
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-12-02 22:52:31 +0100

    Estructura básica inicial
    Creación de páginas vacias

--- batuta/src/app/app.routes.ts
+++ batuta/src/app/app.routes.ts
@@ -11,4 +11,20 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./auth/sign-up/sign-up.page').then( m => m.SignUpPage)
+  },
+  {
+    path: 'account',
+    loadComponent: () => import('./config/account/account.page').then( m => m.AccountPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./config/settings/settings.page').then( m => m.SettingsPage)
+  },
 ];
