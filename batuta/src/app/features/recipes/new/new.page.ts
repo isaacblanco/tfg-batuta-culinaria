55516793697475b6892671c53cf47759552374d4
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-12-29 13:40:04 +0100

    Empieza a fallar el formulario
    NG01203: No value accessor for form control name:

--- batuta/src/app/features/recipes/new/new.page.ts
+++ batuta/src/app/features/recipes/new/new.page.ts
@@ -1,28 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonItemDivider, IonLabel, IonList, IonMenuButton, IonSegmentButton, IonSelectOption, IonTitle, IonToggle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton, IonButtons,
+  IonCol,
+  IonContent, IonGrid, IonHeader, IonIcon,
+  IonItem, IonItemDivider, IonLabel, IonList, IonMenuButton, IonRow, IonSegmentButton,
+  IonSelectOption, IonTitle, IonToggle, IonToolbar
+} from '@ionic/angular/standalone';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 import { SupabaseService } from 'src/app/core/services/supabase.service';
 import { UNITS } from 'src/app/shared/enums/units';
 import { RecipeDTO } from 'src/app/shared/models/recipe-DTO';
 
-
 @Component({
   selector: 'app-new',
   templateUrl: './new.page.html',
   styleUrls: ['./new.page.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NewPage),
+      multi: true,
+    }
+  ],
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink,
-    IonSegmentButton,IonButton,
-    IonSelectOption, IonItemDivider, IonToggle, IonList, 
-    IonButtons, IonContent, IonHeader, IonIcon, IonItem, 
-    IonLabel, IonMenuButton, IonTitle, IonToolbar
-    
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink,
+    IonSegmentButton,
+    IonButton,
+    IonSelectOption,
+    IonItemDivider,
+    IonToggle,
+    IonList,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol
+  ]
 })
 export class NewPage implements OnInit {
+
   units = UNITS;
   recipeId: string | null = null; // ID de la receta (opcional)
   userId: string = ''; // ID del usuario autenticado
@@ -57,13 +86,17 @@ export class NewPage implements OnInit {
     if (userData) {
       this.userId = userData.id;
       this.recipe.user_id = this.userId;
+      console.log("usuario: ", this.userId);
     }
 
     // Comprobar si estamos en modo edición o creación
     this.recipeId = this.route.snapshot.paramMap.get('id');
     if (this.recipeId) {
       this.isEditMode = true;
+      this.recipe.id = parseInt( this.recipeId );
+      console.log("Receta id", this.recipeId);
       this.loadRecipe(this.recipeId); // Cargar receta existente
+
     }
 
     this.loadCategories();
@@ -123,6 +156,15 @@ export class NewPage implements OnInit {
     }
   }
 
+  updateDurationUnit(index: number, isChecked: boolean) {
+    this.recipe.steps[index].durationUnit = isChecked ? 'seconds' : 'minutes';
+  }
+
+  trackByIndex(index: number, item: any): number {
+    return index;
+  }
+  
+
   // Añadir un nuevo ingrediente
   addIngredient() {
     this.recipe.ingredients.push({ name: '', quantity: 1, unit: 'grs' });
@@ -151,11 +193,9 @@ export class NewPage implements OnInit {
   isFormValid(): boolean {
     return (
       !!this.recipe.name &&
-      !!this.recipe.preparation_time &&
-      this.recipe.ingredients.every(
-        (ing) => !!ing.name && !!ing.quantity && !!ing.unit
-      ) &&
-      this.recipe.steps.every((step) => !!step.duration && !!step.instructions)
+      !!this.recipe.preparation_time 
     );
+    
   }
+  
 }
